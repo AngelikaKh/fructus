0a021ad43214150872644bc9c75d5cfd9f82c905
AngelikaKh <angelika1mos@gmail.com>
2021-06-26 21:26:18 +0400

    Initial Commit

---
+++ fructus/fructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fructusApp.swift
+//  fructus
+//
+//  Created by Angelika Khodzhaian on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct fructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
